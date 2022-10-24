06bad49b0dc5c4c7250a9a715a0240da148923d9
Rafael Prieto <rafael.prieto@solera.com>
2022-10-24 10:27:52 +0200

    DB connection and CORS Added

--- questionariobackend/questionariobackend/Program.cs
+++ questionariobackend/questionariobackend/Program.cs
@@ -1,11 +1,29 @@
+using questionariobackend.Data;
+using questionariobackend.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000",
+                                "http://localhost");
+        });
+});
 
 var app = builder.Build();
 
